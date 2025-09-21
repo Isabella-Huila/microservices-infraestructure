628c5aecf7459699886ce407c87a158a1ab80ad6
isabella huila cerom <isahc221004@gmail.com>
2025-09-20 23:57:15 -0500

    corrección nombre de storage account

--- infra_base/main.tf
+++ infra_base/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "rg-terraform"
-    storage_account_name  = "terraformstate12345"
+    storage_account_name  = "mistorageisa"
     container_name        = "tfstate"
     key                   = "infra.terraform.tfstate"
   }
